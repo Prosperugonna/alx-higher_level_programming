7c1d1dc25d8498d85f5a76586365585b5de31c9f
Prosperugonna <prosperugonna@gmail.com>
2023-03-15 04:24:50 -0700

    DONE

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
 
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+    roman_letters = [
+        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+        ['X', 10], ['V', 5], ['I', 1]
+    ]
+    num = 0
+    last = 0
+
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
+
+                last = elem[1]
+
+    return num
