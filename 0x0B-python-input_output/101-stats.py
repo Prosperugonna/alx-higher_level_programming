fd0161d32b504944bcaebdb19bcc10418505115a
Prosperugonna <prosperugonna@gmail.com>
2023-04-12 05:09:05 -0700

    fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,44 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import sys
-import signal
 
-# Initialize counters
-total_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+count = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+size = 0
 
-# Define signal handler to print stats on keyboard interrupt (CTRL + C)
-def print_stats(signal, frame):
-    print("Total file size: File size: {}".format(total_size))
-    for code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print("{}: {}".format(code, count))
-    sys.exit(0)
+try:
+    for line in sys.stdin:
+        count += 1
+        data = line.split()
 
-signal.signal(signal.SIGINT, print_stats)
+        try:
+            size += int(data[-1])
+        except:
+            pass
 
-# Read input line by line
-for i, line in enumerate(sys.stdin):
-    try:
-        ip, _, _, _, _, _, _, _, _, status_code, size = line.split()
-        size = int(size)
-        total_size += size
-        status_code_counts[int(status_code)] += 1
-    except ValueError:
-        continue
-    if (i+1) % 10 == 0:
-        print("Total file size: File size: {}".format(total_size))
-        for code, count in sorted(status_code_counts.items()):
-            if count > 0:
-                print("{}: {}".format(code, count))
+        try:
+            code = data[-2]
+            if code in codes:
+                codes[code] += 1
+        except:
+            pass
+
+        if count % 10 == 0:
+            print("File size: {}".format(size))
+            for code in sorted(codes.keys()):
+                if codes[code] != 0:
+                    print("{}: {}".format(code, codes[code]))
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for code in sorted(codes.keys()):
+        if codes[code] != 0:
+            print("{}: {}".format(code, codes[code]))
+    raise
