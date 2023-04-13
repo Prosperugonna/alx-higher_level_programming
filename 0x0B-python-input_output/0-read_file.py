79fafe5ae9cf69d3c82f7f2a623b804f5dba5dcf
Prosperugonna <prosperugonna@gmail.com>
2023-04-13 08:58:40 -0700

    fix erroe

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-""" This module that contains a function which reads from a file """
+"""read element from the file"""
+
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    """read element to the file
+
+    Args:
+        filename: the file name
+    """
+    with open(filename, encoding="utf-8") as myfile:
+        line = myfile.readlines()
+        for i in line:
+            print(i, end='')
